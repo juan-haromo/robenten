10c34cea2990451cca25c0d517cec4e877da95db
unknown <david.jung@uniat.edu.mx>
2025-11-05 13:36:21 -0600

    Ulti lista

--- Assets/Scripts/Player/Combat/Abilities/Range/RangeUltimate.cs
+++ Assets/Scripts/Player/Combat/Abilities/Range/RangeUltimate.cs
@@ -6,8 +6,10 @@ using System.Collections;
 [CreateAssetMenu(fileName = "RangeUltimate", menuName = "Player/Abilities/Range/Ultimate")]
 public class RangeUltimate : PlayerAbility
 {
-    [SerializeField] private float takeoffForce, takeoffTime, flyTime;
-    private Coroutine flyCoroutine;
+    [SerializeField] private GameObject proyectilePrefab;
+    [SerializeField] private float takeoffForce, takeoffTime, flyTime, attackRange;
+    [SerializeField] private LayerMask worldLayer;
+    private Coroutine flyCoroutine, shootCoroutine;
 
     public override void Activate(Player player)
     {
@@ -22,26 +24,46 @@ public class RangeUltimate : PlayerAbility
         {
             rb.AddForce(Vector3.up * takeoffForce, ForceMode.Impulse);
             if (flyCoroutine == null)
-                flyCoroutine = player.StartCoroutine(FlyFixedUpdate(rb));
+                flyCoroutine = player.StartCoroutine(FlyFixedUpdate(rb, player));
         }
     }
 
-    private IEnumerator FlyFixedUpdate(Rigidbody rb)
+    private IEnumerator ShootFast(Player player)
+    {
+        while (true)
+        {
+            Shoot(player);
+            yield return new WaitForSeconds(CooldownTime);
+        }
+    }
+
+    private void Shoot(Player player)
+    {
+        Vector3 shootTarget;
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        if (Physics.Raycast(ray, out RaycastHit hit, attackRange, worldLayer))
+            shootTarget = hit.point;
+        else
+            shootTarget = ray.origin + ray.direction * attackRange;
+        Vector3 shootDirection = (shootTarget - player.proyectileSpawnpoint.position).normalized;
+        GameObject newArrow = Instantiate(proyectilePrefab, player.proyectileSpawnpoint.position, Quaternion.LookRotation(shootDirection));
+        if (newArrow.TryGetComponent(out ProyectileClass arrowScript))
+            arrowScript.ShootInDirection(shootDirection);
+    }
+
+    private IEnumerator FlyFixedUpdate(Rigidbody rb, Player player)
     {
         yield return new WaitForSeconds(takeoffTime);
         rb.useGravity = false;
         rb.linearVelocity = Vector3.zero;
-        float flyTimeCount = 0f;
-        while (flyTimeCount < flyTime)
-        {
-            
-            flyTimeCount += Time.fixedDeltaTime;
-            yield return new WaitForFixedUpdate();
-        }
+        if (shootCoroutine == null)
+            shootCoroutine = player.StartCoroutine(ShootFast(player));
+        yield return new WaitForSeconds(flyTime);
         flyCoroutine = null;
+        player.StopCoroutine(shootCoroutine);
+        shootCoroutine = null;
         rb.useGravity = true;
-        if (rb.gameObject.TryGetComponent(out Player player))
-            player.canUseAbilities = true;
+        player.canUseAbilities = true;
         yield break;
     }
 }
